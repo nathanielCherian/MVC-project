0c6b884ab56826b9c7ba6878417262367f1afbd7
tonyhieu <amv7668@gmail.com>
2020-10-22 08:57:05 -0700

    started model view control

---
+++ mvc-project/src/control/MathFunctions.java
@@ -0,0 +1,14 @@
+package control;
+
+public class MathFunctions {
+    public static void main(String[] args){
+        System.out.println("Test");
+    }
+
+    public double addition (double arg1, double arg2) {
+        System.out.println("function");
+        return arg1 + arg2;
+    }
+
+
+}
---
+++ mvc-project/src/model/model.js
@@ -0,0 +1,7 @@
+var arg1;
+var arg2;
+
+var control = Packages.control.MathFunctions;
+function test() {
+    document.getElementById("test").innerHTML = control.addition(1, 1);
+}
